fede97f9dbf674f725b7b31036bdabf2c849f663
Elliot Chang <elliotchang126@gmail.com>
2023-08-14 22:12:36 -0700

    Added css to types component

--- frontend/src/components/ListingIndex/ListingIndex.jsx
+++ frontend/src/components/ListingIndex/ListingIndex.jsx
@@ -1,21 +1,27 @@
 import ListingIndexItem from '../ListingIndexItem/ListingIndexItem';
+import ListingTypes from '../ListingTypes/ListingType';
 import './ListingIndex.css'
 
 const ListingIndex = () => {
     return (
-        <div className='listing-index'>
-            <ListingIndexItem />
-            <ListingIndexItem />
-            <ListingIndexItem />
-            <ListingIndexItem />
-            <ListingIndexItem />
-            <ListingIndexItem />
-            <ListingIndexItem />
-            <ListingIndexItem />
-            <ListingIndexItem />
-            <ListingIndexItem />
-            <ListingIndexItem />
-            <ListingIndexItem />
+        <div>
+            <div>
+                <ListingTypes />
+            </div>
+            <div className='listing-index'>
+                <ListingIndexItem />
+                <ListingIndexItem />
+                <ListingIndexItem />
+                <ListingIndexItem />
+                <ListingIndexItem />
+                <ListingIndexItem />
+                <ListingIndexItem />
+                <ListingIndexItem />
+                <ListingIndexItem />
+                <ListingIndexItem />
+                <ListingIndexItem />
+                <ListingIndexItem />
+            </div>
         </div>
     )
 }
