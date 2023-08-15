1dae2bdb30cce1f4f177332f093c99ed52952b4d
Elliot Chang <elliotchang126@gmail.com>
2023-08-14 23:02:53 -0700

    Added placeholder footer. Tried to add favicon

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div>
+            <div>
+                Footer goes here
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
