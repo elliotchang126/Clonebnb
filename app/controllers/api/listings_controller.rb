a26e76b750eff2a3c0180b81074c03df89e104f9
Elliot Chang <elliotchang126@gmail.com>
2023-08-23 10:25:41 -0700

    updated backend index for search

--- app/controllers/api/listings_controller.rb
+++ app/controllers/api/listings_controller.rb
@@ -1,10 +1,16 @@
 class Api::ListingsController < ApplicationController
 
     def index
+        @listings = Listing.all
+
+        if params[:search]
+            @listings = @listings.where("city ILIKE ?", "%#{params[:search]}%")
+        end
+        if params[:state]
+            @listings = @listings.where("state ILIKE ?", "%#{params[:state]}%")
+        end
         if params[:category]
-            @listings = Listing.where("lower(category) = ?", params[:category].downcase)
-        else
-            @listings = Listing.all
+            @listings = @listings.where("lower(category) = ?", params[:category].downcase)
         end
         render :index
     end
