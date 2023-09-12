af079b755d83768dfa9bebd8c85553d62c53e5ac
Elliot Chang <elliotchang126@gmail.com>
2023-09-12 15:45:51 -0700

    added target links for socials

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -10,10 +10,10 @@ const Footer = () => {
                 &copy; 2023 Clonebnb, Inc.
             </div>
             <div className="social-links">
-                <a href="https://github.com/elliotchang126/Clonebnb">
+                <a href="https://github.com/elliotchang126/Clonebnb" target='_blank' >
                     <img src={github} alt="github" className='social-icon'/>
                 </a>
-                <a href="https://www.linkedin.com/in/elliotchang126/">
+                <a href="https://www.linkedin.com/in/elliotchang126/" target='_blank' >
                     <img src={linkedin} alt="linkedin" className='social-icon'/>
                 </a>
             </div>
